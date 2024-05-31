fe6f660477f5576f1e30491a7b6dc465de2d8435
Amo <amm10090@163.com>
2024-05-31 21:42:29 +0800

    icon

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,48 +1,115 @@
 <template>
-  <view class="content">
-    <image class="logo" src="/static/logo.png"></image>
-    <view class="text-area">
-      <text class="title">{{ title }}</text>
+  <view class="container">
+    <view class="header">
+      <text class="title">尤伦斯当代艺术中心</text>
+      <view class="menu">
+        <text>...</text>
+        <image src="/static/icon/more.png" class="icon"></image>
+      </view>
+    </view>
+    <view class="content">
+      <image src="/static/images/exhibition.png" class="exhibition-image"></image>
+      <view class="info">
+        <text class="chinese-title">沉睡者的抵抗</text>
+        <text class="english-title">Resistance of the Sleepers</text>
+        <text class="date">2023.4.30 - 2023.9.6</text>
+      </view>
+    </view>
+    <view class="buttons">
+      <button class="button">在线购票</button>
+      <button class="button">语言导览</button>
+    </view>
+    <view class="footer">
+      <text class="footer-title">精选展览</text>
+      <navigator url="/pages/exhibition/exhibition" class="more-link">更多</navigator>
+    </view>
+    <view class="navbar">
+      <navigator url="/pages/index/index" class="nav-item active">首页</navigator>
+      <navigator url="/pages/explore/explore" class="nav-item">展览</navigator>
+      <navigator url="/pages/events/events" class="nav-item">活动</navigator>
+      <navigator url="/pages/profile/profile" class="nav-item">我的</navigator>
     </view>
   </view>
 </template>
 
-<script>
-export default {
-  data() {
-    return {
-      title: 'Hello',
-    }
-  },
-  onLoad() {},
-  methods: {},
-}
-</script>
-
 <style>
-.content {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+
+.container {
+  @apply bg-white p-4;
 }
 
-.logo {
-  height: 200rpx;
-  width: 200rpx;
-  margin-top: 200rpx;
-  margin-left: auto;
-  margin-right: auto;
-  margin-bottom: 50rpx;
-}
-
-.text-area {
-  display: flex;
-  justify-content: center;
+.header {
+  @apply flex justify-between items-center;
 }
 
 .title {
-  font-size: 36rpx;
-  color: #8f8f94;
+  @apply text-xl font-bold;
+}
+
+.menu {
+  @apply flex items-center;
+}
+
+.icon {
+  @apply w-5 h-5;
+}
+
+.content {
+  @apply mt-4;
+}
+
+.exhibition-image {
+  @apply w-full rounded-lg;
+}
+
+.info {
+  @apply mt-2;
+}
+
+.chinese-title {
+  @apply text-2xl font-bold;
+}
+
+.english-title {
+  @apply text-sm text-gray-500;
+}
+
+.date {
+  @apply text-sm text-gray-500;
+}
+
+.buttons {
+  @apply flex justify-around mt-4;
+}
+
+.button {
+  @apply bg-gray-100 p-2 rounded-lg shadow-md;
+}
+
+.footer {
+  @apply mt-4;
+}
+
+.footer-title {
+  @apply text-lg font-bold;
+}
+
+.more-link {
+  @apply text-sm text-orange-500;
+}
+
+.navbar {
+  @apply fixed bottom-0 left-0 w-full flex justify-around bg-white border-t border-gray-200;
+}
+
+.nav-item {
+  @apply text-center p-2;
+}
+
+.nav-item.active {
+  @apply text-orange-500;
 }
 </style>
